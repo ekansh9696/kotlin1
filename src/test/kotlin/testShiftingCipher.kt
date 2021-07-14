25db90dfc4891a2f6b58fe95d922ca1d2ff1f913
ekansh9696 <ekanshkdmaworld@gmail.com>
2021-07-14 16:13:59 +0530

    increased complexities of the test

--- src/test/kotlin/testShiftingCipher.kt
+++ src/test/kotlin/testShiftingCipher.kt
@@ -2,7 +2,22 @@ import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 
 class ShiftingCipherTests : StringSpec({
-    "Number 3 itself" {
+    "single letter (capital) Shift by 1" {
         val obj : Shift = Shift()
         obj.shiftBy("A",1) shouldBe "B"
-    }})
+
+        }
+    "multiple letters (capital) Shift by 1" {
+        val obj : Shift = Shift()
+        obj.shiftBy("ABCDE",1) shouldBe "BCDEF"
+    }
+    "single letter (capital) Shift by 4" {
+        val obj : Shift = Shift()
+        obj.shiftBy("F",4) shouldBe "J"
+
+    }
+    "multiple letters (capital) Shift by 3" {
+        val obj : Shift = Shift()
+        obj.shiftBy("ABCDE",3) shouldBe "DEFGH"
+    }
+})
